3ea9ccc5617d8d0bea074532bb9d88e7a86c7eda
jamievullo <jvullo@protonmail.com>
2019-12-31 21:35:24 -0500

    Added planets and listener for click event on planets

--- frontend/scripts/planet.js
+++ frontend/scripts/planet.js
@@ -1,14 +1,14 @@
-const planetUrl = `${targetUrl}/planets`
-const moonUrl = `${targetUrl}/moons`
+const planetUrl = `${targetUrl}/planets`;
+const moonUrl = `${targetUrl}/moons`;
 
-const clearForm = document.getElementById('user-form')
-const clearMainImage = document.getElementById('main-pic')
+const clearForm = document.getElementById('user-form');
+const clearMainImage = document.getElementById('main-pic');
 
 function renderPlanets() {
     //clear form and main image
     clearForm.innerHTML = "";
-    clearMainImage.remove()
-    const selectPicElement = document.getElementById('planet-pics')
+    clearMainImage.remove();
+    const selectPicElement = document.getElementById('planet-pics');
     const loadPics = `
     <section id="photos">
         <img id="pic" src="images/Planets/Earth-Main2.jpg" alt="" height="300" width="500">
@@ -22,19 +22,31 @@ function renderPlanets() {
         <img id="pic" src="images/Planets/Saturn-Main-Main.png" alt="" height="300" width="500">      
     </section>`;
 
-    selectPicElement.innerHTML = loadPics
+    selectPicElement.innerHTML = loadPics;
 
-    
     welcomeUser();
-
-
 }
 
 function welcomeUser() {
     const welcomeUserBox = document.getElementById('welcome-user');
     //create variable that displays welcome message based on username and id??
     const welcome =
-    `<div><h1>Welcome, please select a Planet by clicking on its image</h1></div>`;
+    `<div><h1><center>Welcome, please select a Planet 
+    by clicking on its image</center></h1></div>`;
 
     welcomeUserBox.innerHTML = welcome;
+    selectPlanet();
 }
+
+function selectPlanet() {
+    // set variable to pic id for planets
+    const selectFromPlanets = document.querySelectorAll('#pic')
+    // iterate over pic collection and add event listener for which pic is being
+    // selected(event.target). Can use planet.target also since it gets iterated over.
+    selectFromPlanets.forEach(planet => {
+        planet.addEventListener('click', e => {
+        console.log(e.target);
+        })
+    })
+}
+
