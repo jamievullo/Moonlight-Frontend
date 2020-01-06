bb097f9519cf80386bccf0190891c7322a42e095
jamievullo <jvullo@protonmail.com>
2020-01-06 15:15:30 -0500

    Added more content to seed files.

--- backend/app/controllers/moons_controller.rb
+++ backend/app/controllers/moons_controller.rb
@@ -2,8 +2,8 @@ class MoonsController < ApplicationController
 
     def index
         moons = Moon.all
-        if params[:planet_id]
-            moons = Planet.find_by(id: params[:planet_id]).moons
+        if params[:planet_id] #if planet id exists
+            moons = Planet.find_by(id: params[:planet_id]).moons #finds planets moons with planet id
         else
             moons = Moon.all
         end
